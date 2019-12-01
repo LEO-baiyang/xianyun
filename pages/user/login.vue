25302293c173a24fe468a36d4bf351aa3df814cf
LEO-baiyang <15750728179@163.com>
2019-12-01 14:18:01 +0800

    01-登录页面基本结构搭建完成

---
+++ pages/user/login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <el-row type="flex" justify="space-between" align="middle" class="main">
+      <div class="form-wrapper">
+        <el-row type="flex" justify="center" class="tabs">
+          <span v-for="(value,index) in ['登录','注册']" :key="index">{{value}}</span>
+          <!-- <span>注册</span> -->
+        </el-row>
+        
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.container {
+  background: url(http://157.122.54.189:9095/assets/images/th03.jfif) center 0;
+  height: 700px;
+  min-width: 1000px;
+  .main {
+    width: 1000px;
+    height: 100%;
+    margin: 0 auto;
+    position: relative;
+
+    .form-wrapper {
+      width: 400px;
+      margin: 0 auto;
+      background: #fff;
+      box-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
+      overflow: hidden;
+
+      .tabs {
+        span {
+          display: block;
+          width: 50%;
+          height: 50px;
+          box-sizing: border-box;
+          border-top: 2px #eee solid;
+          background: #eee;
+          line-height: 48px;
+          text-align: center;
+          cursor: pointer;
+          color: #666;
+
+          &.active {
+            color: orange;
+            border-top-color: orange;
+            background: #fff;
+            font-weight: bold;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
