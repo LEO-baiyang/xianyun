15679340858974c3398e41fa63eb367205e98c5a
LEO-baiyang <15750728179@163.com>
2019-12-06 19:33:53 +0800

    11-机票列表头部组件搭建完成

---
+++ components/air/flightsListHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flightListTitle">
+    <el-row type="flex" justify="space-between">
+      <el-col :span="6" class="flights-content">航空信息</el-col>
+      <el-col :span="6" class="flights-content">起飞时间</el-col>
+      <el-col :span="6" class="flights-content">到达时间</el-col>
+      <el-col :span="6" class="flights-content">价格</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.flightListTitle {
+  padding: 0 15px;
+  border: 1px #ddd solid;
+  background: #f6f6f6;
+  height: 38px;
+  line-height: 38px;
+  color: #666;
+  margin-bottom: 10px;
+  font-size: 12px;
+  .flights-content {
+    text-align: center;
+  }
+}
+</style>
---
+++ pages/air/flights.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <el-row type="flex" justify="space-between">
+      <!-- 主体  -->
+      <el-col class="flightsList" :span="18">
+        <!-- 过滤筛选 -->
+        <div></div>
+        <!-- 列表头部 -->
+        <FlightsListHeader />
+        <!-- 列表内容 -->
+        <div></div>
+      </el-col>
+      <!-- 侧边 -->
+      <el-col class="aside" :span="6"></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// 引入列表头部部分
+import FlightsListHeader from "@/components/air/flightsListHeader.vue";
+export default {
+  components: {
+    FlightsListHeader
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 1000px;
+  margin: 20px auto;
+  .flightsList {
+    // width: 745px;
+    font-size: 14px;
+    // background-color: orange;
+    margin-right: 15px;
+  }
+  .aside {
+    // width: 240px;  
+    height: 300px;
+    background-color: pink;
+  }
+}
+</style>
